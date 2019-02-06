08c0310043058cde21bfdbeaf47825e152f958a9
kanikas01 <larvakurt@yahoo.com>
2019-02-05 19:05:06 -0800

    Add basic form submission handler (console log output only) and input validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,12 +12,20 @@ $(document).ready(function () {
 
   var isFavoritesVisible = true;
 
+  // Regex patterns for validating user input
+  patterns = {
+    textInput: /\w+/
+  };
+
 
   // ---------- Selectors ---------- //
 
   selectors = {
     hideFavoritesButton: '#hide-favorites',
-    formSubmitButton: '#form-submit'
+    formSubmitButton: '#form-submit',
+    userName: '#name',
+    userCity: '#city',
+    chosenCategory: '#category'
   };
 
 
@@ -45,6 +53,35 @@ $(document).ready(function () {
   // Form submit handler
   $(selectors.formSubmitButton).on('click', function (event) {
     event.preventDefault();
+    // Get user input
+    var name = $(selectors.userName).val().trim();
+    var city = $(selectors.userCity).val().trim();
+    var category; 
+    // user must choose a category or the .trim method will throw an error
+    // if no value is chosen then category is set to an empty string
+    if ( $(selectors.chosenCategory).val() ) {
+      category = $(selectors.chosenCategory).val().trim();
+    }
+    else {
+      category = '';
+    }
+
+    if (!patterns.textInput.test(name)) {
+      // $(selectors.modalBody).text('Train Name must not be empty.');
+      // $(selectors.modalAlert).modal('show');
+      console.log('Name field cannot be empty.')
+      return;
+    }
+    if (!patterns.textInput.test(city)) {
+      console.log('City field cannot be empty.')
+      return;
+    }
+    if (!patterns.textInput.test(category)) {
+      console.log('You must choose a category.')
+      return;
+    }
+
+    console.log(name, city, category);
   });
 
 
