28713eb11500de4381c83b70bb0ba976e7b2c408
kanikas01 <larvakurt@yahoo.com>
2019-02-05 17:28:03 -0800

    Move test javascript to external file

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+
+// Wait until DOM is loaded
+$(document).ready(function () {
+
+  // Initialize materialize components
+  $('.collapsible').collapsible();
+  $('.dropdown-trigger').dropdown();
+  $('select').formSelect();
+
+
+  // ---------- Global variables ---------- //
+
+  var isFavoritesVisible = true;
+
+
+  // ---------- Selectors ---------- //
+
+  selectors = {
+    hideFavoritesButton: '#hide-favorites'
+  };
+
+
+  // ---------- Classes ---------- //
+
+  classes = {};
+
+
+  // ---------- Events ---------- //
+
+  // Click button to hide or show favorites section
+  $(selectors.hideFavoritesButton).on('click', function (event) {
+    event.preventDefault();
+    $("#mylist").toggleClass("scale-out");
+    if (isFavoritesVisible) {
+      isFavoritesVisible = false;
+      $("#hide-favorites").text("Show Favorites");
+    }
+    else {
+      isFavoritesVisible = true;
+      $("#hide-favorites").text("Hide Favorites");
+    }
+  });
+
+  
+  // ---------- Helpers ---------- //
+
+});
